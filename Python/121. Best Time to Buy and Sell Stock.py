fc677088ee711e5260eb7795405f57e5f9668e67
Vincent <18235140150@163.com>
2018-05-27 19:57:24 +0800

    Create 121. Best Time to Buy and Sell Stock.py

---
+++ Python/121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,19 @@
+# Say you have an array for which the ith element is the price of a given stock on day i.
+
+# If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+# Note that you cannot sell a stock before you buy one.
+
+class Solution:
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        minprice, maxprofit = float('inf'), 0
+        for price in prices:
+            if price < minprice:
+                minprice = price
+            elif price - minprice > maxprofit:
+                maxprofit = price - minprice
+        return maxprofit
